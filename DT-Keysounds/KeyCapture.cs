dac76ff9162700f8e9fc3395919e9dbca5826e6c
Charles Oliver <charles@iwritecodein.net>
2014-01-09 06:53:13 +0000

    Inital commit.
    Keyboard hook in place, as is logic to prevent callback from firing when a key is being held down.

---
+++ DT-Keysounds/KeyCapture.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace DT_Keysounds
+{
+    public class KeyCapture
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+
+        private static readonly List<int> KeysDown = new List<int>();
+
+        private static readonly LowLevelKeyboardProc Proc = HookCallback;
+        private static IntPtr _hookId = IntPtr.Zero;
+
+        public static void Main()
+        {
+            _hookId = SetHook(Proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookId);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (var currentProcess = Process.GetCurrentProcess())
+            using (var currentModule = currentProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(currentModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                var keycode = Marshal.ReadInt32(lParam);
+
+                if (wParam == (IntPtr)WM_KEYUP)
+                    KeysDown.RemoveAll(k => k == keycode);
+
+                if (wParam == (IntPtr)WM_KEYDOWN)
+                {
+                    if (!KeysDown.Contains(keycode))
+                        Console.WriteLine((Keys)keycode);
+
+                    KeysDown.Add(keycode);
+                }
+
+            }
+
+            return CallNextHookEx(_hookId, nCode, wParam, lParam);
+        }
+
+        #region Dll Imports
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        #endregion
+    }
+}
